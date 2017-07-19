a183dba7517943f58f087a3145b569d804a026cc
Anatolijus <join.eu.pn@gmail.com>
2017-07-19 10:54:08 +0300

    create queue using array with max size test

---
+++ src/SimpleQueue.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Anatolijus on 2017-07-19.
+ */
+public class SimpleQueue {
+
+    int[] queue;
+
+    public SimpleQueue(int maxSize) {
+
+    }
+
+}
---
+++ test/SimpleQueueTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+/**
+ * Created by Anatolijus on 2017-07-19.
+ */
+public class SimpleQueueTest {
+
+    SimpleQueue queue = new SimpleQueue(25);
+
+    @Test public void canCreateQueueArrayWithMaxLength() {
+        assertEquals(25, queue.queue.length);
+    }
+
+}

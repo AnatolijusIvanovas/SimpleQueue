6798109c2ec34533f603d9ffc2a222be3a65d63d
Anatolijus <join.eu.pn@gmail.com>
2017-07-19 11:33:49 +0300

    SimpleQueueTest formated

--- test/SimpleQueueTest.java
+++ test/SimpleQueueTest.java
@@ -7,15 +7,10 @@ public class SimpleQueueTest {
 
     SimpleQueue queue = new SimpleQueue(25);
 
-    @Test public void canCreateQueueArrayWithMaxLength() {
-        assertEquals(25, queue.queue.length);
-    }
-    @Test public void canCreateQueueWithMaxSizeParameter() {
-        assertEquals(25, queue.maxSize);
-    }
+    @Test public void canCreateQueueArrayWithMaxLength() { assertEquals(25, queue.queue.length); }
+    @Test public void canCreateQueueWithMaxSizeParameter() { assertEquals(25, queue.maxSize); }
     @Test public void canCreateFirstElementIndex() { assertEquals(0, queue.firstElement); }
     @Test public void canCreateLastElementIndex() { assertEquals(-1, queue.lastElement); }
     @Test public void canCreateQueueWithNumberOfElementsParameter() { assertEquals(0, queue.numberOfElements); }
-
 
 }
